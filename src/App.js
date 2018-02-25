2c58d48e155dd135a07c55fc65a9aea343113e97
VorontsovSA <vorontsov.s.a@gmail.com>
2018-02-25 22:30:54 +1000

    wip

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.css";
 
-import { Navbar, Grid, Row, Col, Form, FormGroup, ControlLabel, FormControl, Table, Well } from "react-bootstrap";
+import {Navbar, Grid, Row, Col, Form, FormGroup, ControlLabel, FormControl, Table, Well} from "react-bootstrap";
+
+function FieldGroup({id, label, ...props}) {
+  return (
+    <FormGroup controlId={id} md={1}
+               sm={1}>
+      <FormControl {...props} />
+    </FormGroup>
+  );
+}
 
 class App extends Component {
   constructor() {
@@ -13,10 +22,11 @@ class App extends Component {
 
     this.state = {
       activePlace: 0,
-      earning: 0.00009,
+      earning: 0.00017,
       rate: 10000,
       rePurchase: 40,
-      poolPrice: 500,
+      poolsCount: 1,
+      poolPrice: 1000,
       poolLife: 0,
       maxEarning: 0,
       days: [],
@@ -25,37 +35,38 @@ class App extends Component {
   }
 
   handleChange() {
-    this.setState({ earning: this.earning.value });
-    this.setState({ rate: this.rate.value });
-    this.setState({ rePurchase: this.rePurchase.value });
-    this.setState({ poolPrice: this.poolPrice.value });
+    this.setState({earning: this.earning.value});
+    this.setState({rate: this.rate.value});
+    this.setState({rePurchase: this.rePurchase.value});
+    this.setState({poolPrice: this.poolPrice.value});
+    this.setState({poolsCount: this.poolsCount.value});
     this.updateValues();
   }
 
   updateValues() {
-    var earning = this.earning.value;
-    var rate = this.rate.value;
-    var rePurchase = this.rePurchase.value;
-    var poolPrice = this.poolPrice.value;
-    var poolSharePrice = this.poolPrice.value*0.05/rate;
-    var rePurchaseAccumulator = 0;
-    var days = [];
-    var currentEarning = 0;
-    var poolShare = 1;
-    var pools = [];
-    var realEarning = 0;
-    var rePurchaseShare = 0;
-    var mainPoints = [];
+    let earning = this.earning.value;
+    let rate = this.rate.value;
+    let rePurchase = this.rePurchase.value;
+    let poolPrice = this.poolPrice.value;
+    let poolSharePrice = this.poolPrice.value * 0.05 / rate;
+    let rePurchaseAccumulator = 0;
+    let days = [];
+    let currentEarning = 0;
+    let poolShare = Number(this.poolsCount.value);
+    let pools = [];
+    let realEarning = 0;
+    let rePurchaseShare = 0;
+    let mainPoints = [];
     pools[0] = {
       lastDay: 1000,
-      share: 1
+      share: this.poolsCount.value
     };
-    var i = 1;
-    var maxI = 2000;
-    var gotPoolPrice = false;
+    let i = 1;
+    let maxI = 2000;
+    let gotPoolPrice = false;
     while (i <= 7300 && poolShare.toFixed(2) > 0) {
       if (pools.length > 0 && pools[0].lastDay < i) {
-        var pool = pools.shift();
+        let pool = pools.shift();
         poolShare -= pool.share;
       }
       if (rePurchaseAccumulator > poolSharePrice) {
@@ -84,15 +95,15 @@ class App extends Component {
           name: '2000 days'
         });
       }
-      if (!gotPoolPrice && poolPrice <= currentEarning*rate) {
+      if (!gotPoolPrice && poolPrice <= currentEarning * rate) {
         gotPoolPrice = true;
         mainPoints.push({
           i: i,
           name: i + ' days'
         });
       }
-      realEarning = poolShare*earning;
-      rePurchaseShare = realEarning*rePurchase/100;
+      realEarning = poolShare * earning;
+      rePurchaseShare = realEarning * rePurchase / 100;
       rePurchaseAccumulator += rePurchaseShare;
       currentEarning += realEarning - rePurchaseShare;
 
@@ -105,11 +116,12 @@ class App extends Component {
         };
       }
       i++;
-    };
-    this.setState({ poolLife: i - 1 });
-    this.setState({ mainPoints: mainPoints });
-    this.setState({ maxEarning: currentEarning });
-    this.setState({ days: days });
+    }
+    ;
+    this.setState({poolLife: i - 1});
+    this.setState({mainPoints: mainPoints});
+    this.setState({maxEarning: currentEarning});
+    this.setState({days: days});
   }
 
   componentDidMount() {
@@ -129,51 +141,71 @@ class App extends Component {
         </Navbar>
         <Grid>
           <Row>
-            <Col md={4} sm={4}>
+            <Col md={6} sm={6}>
               <Form>
                 <FormGroup>
                   <ControlLabel><strong>Daily earning</strong></ControlLabel>
                   <FormControl
-                    inputRef={ref => { this.earning = ref; }}
+                    inputRef={ref => {
+                      this.earning = ref;
+                    }}
                     value={this.state.earning}
                     type="text"
                     placeholder=""
                     onChange={this.handleChange}
                   />
-                  <FormControl.Feedback />
                 </FormGroup>
                 <FormGroup>
                   <ControlLabel><strong>BTC price $</strong></ControlLabel>
                   <FormControl
-                    inputRef={ref => { this.rate = ref; }}
+                    inputRef={ref => {
+                      this.rate = ref;
+                    }}
                     value={this.state.rate}
                     type="text"
                     placeholder=""
                     onChange={this.handleChange}
                   />
-                  <FormControl.Feedback />
                 </FormGroup>
                 <FormGroup>
                   <ControlLabel><strong>re-purchase %</strong></ControlLabel>
                   <FormControl
-                    inputRef={ref => { this.rePurchase = ref; }}
+                    inputRef={ref => {
+                      this.rePurchase = ref;
+                    }}
                     value={this.state.rePurchase}
                     type="number"
                     placeholder=""
                     onChange={this.handleChange}
                   />
-                  <FormControl.Feedback />
                 </FormGroup>
-                <FormGroup>
-                  <ControlLabel><strong>poolPrice</strong></ControlLabel>
+                <FormGroup controlId="pool1Count">
+                  <ControlLabel>Pool#1</ControlLabel>{' '}
                   <FormControl
-                    inputRef={ref => { this.poolPrice = ref; }}
+                    componentClass="select"
+                    inputRef={ref => {
+                      this.poolPrice = ref;
+                    }}
                     value={this.state.poolPrice}
+                    placeholder=""
+                    onChange={this.handleChange}
+                  >
+                    <option value="500">500</option>
+                    <option value="1000">1000</option>
+                    <option value="2000">2000</option>
+                  </FormControl>
+                </FormGroup>{' '}
+                <FormGroup controlId="pool2Count">
+                  <ControlLabel>Pools count</ControlLabel>{' '}
+                  <FormControl
+                    inputRef={ref => {
+                      this.poolsCount = ref;
+                    }}
+                    value={this.state.poolsCount}
                     type="text"
                     placeholder=""
                     onChange={this.handleChange}
                   />
-                  <FormControl.Feedback />
                 </FormGroup>
               </Form>
             </Col>
@@ -182,14 +214,16 @@ class App extends Component {
                 {this.state.mainPoints.map((point, i) => {
                   return (
                     <span key={i}>
-                      {point.name} ({(point.i/365).toFixed(2)} year) 
+                      {point.name} ({(point.i / 365).toFixed(2)} year)
                       / {this.state.days.length >= point.i ? (this.state.days[point.i].earning).toFixed(6) + ' BTC ' : ''}
-                      / {this.state.days.length >= point.i ? (this.state.days[point.i].earning*this.rate.value).toFixed(2) + ' $' : ''}
-                      <br />
+                      / {this.state.days.length >= point.i ? (this.state.days[point.i].earning * this.rate.value).toFixed(2) + ' $' : ''}
+                      <br/>
                     </span>
                   );
                 })}
-                {this.state.poolLife} days ({(this.state.poolLife/365).toFixed(2)} year) / {(this.state.maxEarning).toFixed(6) + ' BTC'} / {(this.state.maxEarning*this.state.rate).toFixed(2) + ' $'}
+                {this.state.poolLife} days ({(this.state.poolLife / 365).toFixed(2)} year)
+                / {(this.state.maxEarning).toFixed(6) + ' BTC'}
+                / {(this.state.maxEarning * this.state.rate).toFixed(2) + ' $'}
               </Well>
             </Col>
           </Row>
@@ -197,26 +231,26 @@ class App extends Component {
             <Col md={12} sm={12}>
               <Table striped bordered condensed hover>
                 <thead>
-                  <tr>
-                    <th>#</th>
-                    <th>Pool share</th>
-                    <th>rePurchase acc</th>
-                    <th>Earning BTC</th>
-                    <th>Earning $</th>
-                  </tr>
+                <tr>
+                  <th>#</th>
+                  <th>Pool share</th>
+                  <th>rePurchase acc</th>
+                  <th>Earning BTC</th>
+                  <th>Earning $</th>
+                </tr>
                 </thead>
                 <tbody>
-                  {this.state.days.map((day, i) => {
-                    return (
-                      <tr key={i}>
-                        <td>{day.number} ({(day.number/365).toFixed(2)})</td>
-                        <td>{(day.poolShare).toFixed(2)}</td>
-                        <td>{(day.rePurchaseAccumulator*this.rate.value).toFixed(2)}</td>
-                        <td>{(day.earning).toFixed(9)}</td>
-                        <td>{(day.earning*this.rate.value).toFixed(2)}</td>
-                      </tr>
-                    );
-                  })}
+                {this.state.days.map((day, i) => {
+                  return (
+                    <tr key={i}>
+                      <td>{day.number} ({(day.number / 365).toFixed(2)})</td>
+                      <td>{(day.poolShare).toFixed(2)}</td>
+                      <td>{(day.rePurchaseAccumulator * this.rate.value).toFixed(2)}</td>
+                      <td>{(day.earning).toFixed(9)}</td>
+                      <td>{(day.earning * this.rate.value).toFixed(2)}</td>
+                    </tr>
+                  );
+                })}
                 </tbody>
               </Table>
             </Col>
